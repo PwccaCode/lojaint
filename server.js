6c53b6e5665493b696fb1e8843ba167fc4a9b0c7
LeoH <51799102+PwccaCode@users.noreply.github.com>
2022-07-23 18:44:01 -0300

    commit update

--- server.js
+++ server.js
@@ -1,42 +1,97 @@
 const express = require('express')
 const app = express();
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const res = require("express/lib/response");
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const res = require('express/lib/response');
+const ejs = require('ejs');
 const { request } = require('http');
+const path = require('path');
 
-app.use(bodyParser.urlencoded({extended: true}));
 
-mongoose.connect("mongodb+srv://lojaint:leo123456@lojaint.fwxfp1f.mongodb.net/lojaint")
 
-app.get("/", function(req,res) {
-    res.sendFile(__dirname + "/index.html")
+dotenv.config({path:'config.env'})
+const PORT = process.env.PORT || 8080
+
+//log request
+app.use(morgan('tiny'));
+
+//parse na request
+app.use(bodyparser.urlencoded({ extended: true}))
+
+//setar view engine
+app.set("view engine", "ejs")
+
+//carregar assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// mongoose.connect("mongodb+srv://lojaint:leo123456@lojaint.fwxfp1f.mongodb.net/lojaint")
+
+// //schema
+
+// const lojaSchema = {
+//     nome: String,
+//     fabric: String,
+//     categ: String,
+//     quant: String,
+//     val: String
+// }
+
+
+
+// const Loja = mongoose.model('Loja', lojaSchema);
+
+// app.get("/", (req,res) => {
+//     Loja.find({}, function(err, loja) {
+//         res.render('index.ejs', {
+//             listaProdu: loja
+//         })
+//     })
+// })
+
+
+// app.post("/", function(req, res) {
+//     let newLoja = new Loja({
+//         nome: req.body.nome,
+//         fabric: req.body.fabric,
+//         categ: req.body.categ,
+//         quant: req.body.quant,
+//         val: req.body.val
+//     });
+//     newLoja.save();
+//     res.redirect("/");
+// })
+
+
+app.get('/', (req, res) => {
+    res.render('index');
 })
 
-//schema
-
-const lojaSchema = {
-    nome: String,
-    fabric: String,
-    categ: String,
-    quant: String,
-    val: String
-}
-
-const Loja = mongoose.model("Loja", lojaSchema);
-
-app.post("/", function(req, res) {
-    let newLoja = new Loja({
-        nome: req.body.nome,
-        fabric: req.body.fabric,
-        categ: req.body.categ,
-        quant: req.body.quant,
-        val: req.body.val
-    });
-    newLoja.save();
-    res.redirect("/");
+app.get('/novoprod', (req, res) => {
+    res.render('novoprod');
 })
 
-app.listen(3000, function() {
-    console.log("server na 3000")
+app.get('/updateprod', (req, res) => {
+    res.render('updateprod');
 })
+
+app.listen(PORT, function() {
+     console.log("server na")
+ })
